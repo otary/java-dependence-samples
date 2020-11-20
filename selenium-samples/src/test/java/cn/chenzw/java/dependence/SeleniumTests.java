b24986d0794588ffac6a93c3a3f7e04d0780e6e2
chenzw <656469722@qq.com>
2020-11-20 09:18:06 +0800

    update

--- selenium-samples/src/test/java/cn/chenzw/java/dependence/SeleniumTests.java
+++ selenium-samples/src/test/java/cn/chenzw/java/dependence/SeleniumTests.java
@@ -4,8 +4,10 @@ import cn.chenzw.toolkit.commons.ProjectUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.openqa.selenium.Proxy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.phantomjs.PhantomJSDriver;
 
@@ -16,15 +18,18 @@ public class SeleniumTests {
 
     @Test
     public void testChromeDriver() {
-        System.setProperty("webdriver.chrome.driver", ProjectUtils.getClassPath() + "driver/chrome/chromedriver.exe");
-        WebDriver webDriver = new ChromeDriver();
+        System.setProperty("webdriver.chrome.driver", ProjectUtils.getClassPath() + "driver/chrome/86/chromedriver.exe");
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--headless");
+        chromeOptions.addArguments("--disable-gpu");
+        WebDriver webDriver = new ChromeDriver(chromeOptions);
         webDriver.get("https://passport.jd.com/new/login.aspx");
         System.out.println(webDriver.getPageSource());
         webDriver.quit();
     }
 
     @Test
-    public void testPhantomJsDriver() throws InterruptedException {
+    public void testPhantomJsDriver() {
         System.setProperty("phantomjs.binary.path", ProjectUtils.getClassPath() + "driver/phantomjs/phantomjs.exe");
         WebDriver webDriver = new PhantomJSDriver();
         // 与浏览器同步非常重要，必须等待浏览器加载完毕
@@ -38,6 +43,26 @@ public class SeleniumTests {
     @Test
     public void testFirefoxDriver() {
         WebDriver webDriver = new FirefoxDriver();
+        webDriver.get("https://passport.jd.com/new/login.aspx");
+        System.out.println(webDriver.getPageSource());
+        webDriver.quit();
+    }
+
+
+    /**
+     * 使用代理
+     */
+    @Test
+    public void testProxy() {
+        System.setProperty("webdriver.chrome.driver", ProjectUtils.getClassPath() + "driver/chrome/86/chromedriver.exe");
+        ChromeOptions chromeOptions = new ChromeOptions();
+
+        // 设置代理
+        String proxyServer = "192.168.13.19:7777";
+        Proxy proxy = new Proxy().setHttpProxy(proxyServer).setSslProxy(proxyServer);
+        chromeOptions.setProxy(proxy);
+
+        WebDriver webDriver = new ChromeDriver(chromeOptions);
         webDriver.get("https://passport.jd.com/new/login.aspx");
         System.out.println(webDriver.getPageSource());
         webDriver.quit();
