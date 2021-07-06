79973201046cc7beb273554061407c7e672ecce1
chenzw <656469722@qq.com>
2021-07-06 09:31:23 +0800

    update

---
+++ elasticsearch-sql-samples/src/test/java/cn/chenzw/dependence/elasticsearch/sql/ElasticSearchSqlTests.java
@@ -0,0 +1,23 @@
+package cn.chenzw.dependence.elasticsearch.sql;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import io.github.iamazy.elasticsearch.dsl.sql.ElasticSql2DslParser;
+import io.github.iamazy.elasticsearch.dsl.sql.model.ElasticSqlParseResult;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@Slf4j
+@RunWith(JUnit4.class)
+public class ElasticSearchSqlTests {
+
+    @Test
+    public void testParser() throws JsonProcessingException {
+        ElasticSql2DslParser elasticSql2DslParser = new ElasticSql2DslParser();
+        ElasticSqlParseResult result = elasticSql2DslParser.parse("select name, id, shi from users where id = 1");
+
+        log.info(" => {}", result.toPrettyDsl(result.getSearchRequest()));
+        log.info("表名 => {}", result.getIndices());
+    }
+}
