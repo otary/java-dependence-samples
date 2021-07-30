3c240e9c89d8069aeffc267e42402efa64efcf65
chenzw <656469722@qq.com>
2021-07-30 15:38:18 +0800

    update

--- webmagic-samples/webmagic-basic-samples/src/test/java/cn/chenzw/java/dependence/webmagic/processor/Yn887PageProcessor.java
+++ webmagic-samples/webmagic-basic-samples/src/test/java/cn/chenzw/java/dependence/webmagic/processor/Yn887PageProcessor.java
@@ -1,5 +1,9 @@
 package cn.chenzw.java.dependence.webmagic.processor;
 
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
 import us.codecraft.webmagic.Page;
 import us.codecraft.webmagic.Site;
@@ -8,18 +12,34 @@ import us.codecraft.webmagic.pipeline.ConsolePipeline;
 import us.codecraft.webmagic.processor.PageProcessor;
 import us.codecraft.webmagic.selector.Selectable;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.util.Base64;
 import java.util.List;
+import java.util.UUID;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Yn887PageProcessor implements PageProcessor {
 
+    private Pattern suffixPattern = Pattern.compile(".*\\.(jpg|png|jpeg)");
+
+    private static final String IMAGE_ROOT_PATH = "cartoon-image";
+
+
+    private OkHttpClient okHttpClient = new OkHttpClient.Builder().build();
+
     @Override
     public void process(Page page) {
-        System.out.println(page.getHtml());
+        // System.out.println(page.getHtml());
         /*if (page.getUrl().toString().equals("http://www.yn887.com/wanjie/")) {
             processListPage(page);
         } else if (page.getUrl().regex("http://www.yn887.com/ynmh/\\d+/") != null) {
             processItemPage(page);
         }*/
+
+        processChapterPage(page);
     }
 
     public void processListPage(Page page) {
@@ -60,7 +80,42 @@ public class Yn887PageProcessor implements PageProcessor {
 
             page.addTargetRequest("http://www.yn887.com/" + href);
         }
+    }
 
+    public void processChapterPage(Page page) {
+        String imagesBase64 = page.getHtml().regex("qTcms_S_m_murl_e=\"(.*?)\";").get();
+        String images = new String(Base64.getMimeDecoder().decode(imagesBase64));
+        String[] imagesUrls = images.split("\\$qingtiandy\\$");
+
+        for (String imageUrl : imagesUrls) {
+            Request request = new Request.Builder().url(imageUrl).build();
+
+            try (Response response = okHttpClient.newCall(request).execute();
+                 InputStream is = response.body().byteStream();
+                 FileOutputStream fos = new FileOutputStream(buildImageFile(imageUrl))
+            ) {
+                IOUtils.copy(is, fos);
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    private String getImageSuffix(String url) {
+        Matcher matcher =
+                suffixPattern.matcher(url);
+        if (matcher.find()) {
+            return matcher.group(1);
+        }
+        return "jpg";
+    }
+
+    private File buildImageFile(String imageUrl) {
+        String imageSuffix = getImageSuffix(imageUrl);
+        String imageRandomName = UUID.randomUUID().toString();
+
+
+        return new File(IMAGE_ROOT_PATH + imageRandomName + "." + imageSuffix);
     }
 
     @Override
@@ -74,7 +129,7 @@ public class Yn887PageProcessor implements PageProcessor {
     public static void main(String[] args) {
         Spider.create(new Yn887PageProcessor())
                 .addPipeline(new ConsolePipeline())
-               // .addUrl("http://www.yn887.com/wanjie/")
+                // .addUrl("http://www.yn887.com/wanjie/")
                 .addUrl("http://www.yn887.com/ynmh/29947/627622.html?p=4")
                 .run();
     }
