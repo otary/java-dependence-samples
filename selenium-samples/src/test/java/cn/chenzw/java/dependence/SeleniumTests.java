b15e4276832a9957792152d9e765626c5298cca4
chenzw <656469722@qq.com>
2022-02-23 10:32:30 +0800

    update

--- selenium-samples/src/test/java/cn/chenzw/java/dependence/SeleniumTests.java
+++ selenium-samples/src/test/java/cn/chenzw/java/dependence/SeleniumTests.java
@@ -2,6 +2,7 @@ package cn.chenzw.java.dependence;
 
 import cn.chenzw.toolkit.commons.ProjectUtils;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.FileUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
@@ -9,11 +10,14 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.phantomjs.PhantomJSDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.Collections;
 import java.util.concurrent.TimeUnit;
 
@@ -23,7 +27,7 @@ public class SeleniumTests {
 
     @Test
     public void testChromeDriver() {
-        System.setProperty("webdriver.chrome.driver", ProjectUtils.getClassPath() + "driver/chrome/84/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", ProjectUtils.getClassPath() + "driver/chrome/97/chromedriver.exe");
         // 配置Chrome属性
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.addArguments("--headless");
@@ -86,12 +90,19 @@ public class SeleniumTests {
     }
 
     @Test
-    public void testDemo() throws InterruptedException {
+    public void testAll() throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", ProjectUtils.getClassPath() + "driver/chrome/84/chromedriver.exe");
         WebDriver webDriver = new ChromeDriver();
 
         // 加载某个页面
         webDriver.get("https://passport.jd.com/new/login.aspx");
+
+        //页面最大化
+        // webDriver.manage().window().maximize();
+
+        // 获取窗口大小
+        // webDriver.manage().window().getSize();
+
 
         // 等待10s，让页面加载完成
         WebDriverWait wait = new WebDriverWait(webDriver, 10);
@@ -102,6 +113,10 @@ public class SeleniumTests {
         // 获取页面元素对象
         WebElement element = webDriver.findElement(By.xpath("//div[@class='login-tab login-tab-r']"));
         element.click();
+
+        // 执行js并获取返回值
+       // Long width = (Long) ((JavascriptExecutor) webDriver).executeScript("return document.body.scrollWidth;");
+       // Long height = (Long) ((JavascriptExecutor) webDriver).executeScript("return document.body.scrollHeight;");
 
         // 暂停1s
         // Thread.sleep(1000L);
@@ -121,6 +136,36 @@ public class SeleniumTests {
 
         webDriver.close();
 
+    }
+
+    @Test
+    public void testScreenshot() throws InterruptedException, IOException {
+        System.setProperty("webdriver.chrome.driver", ProjectUtils.getClassPath() + "driver/chrome/97/chromedriver.exe");
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--headless");
+        chromeOptions.addArguments("--disable-gpu");
+        chromeOptions.addArguments("--no-sandbox");
+        chromeOptions.addArguments("--disable-dev-shm-usage");
+        chromeOptions.addArguments("--start-maximized");
+        // 指定页面大小
+       // chromeOptions.addArguments("--window-size=1280,4300");
+
+        WebDriver webDriver = new ChromeDriver(chromeOptions);
+        webDriver.get("https://www.baidu.com");
+
+        // 设置自适应最大窗口
+        Long width = (Long) ((JavascriptExecutor) webDriver).executeScript("return document.body.scrollWidth;");
+        Long height = (Long) ((JavascriptExecutor) webDriver).executeScript("return document.body.scrollHeight;");
+        webDriver.manage().window().setSize(new Dimension(width.intValue(), height.intValue()));
+
+        Thread.sleep(2000); // 等待2s，让动画运行结束
+
+        // 截取全屏
+        File screenshotFile = ((TakesScreenshot)webDriver).getScreenshotAs(OutputType.FILE);
+        // 保存为图片
+        FileUtils.copyFile(screenshotFile, new File("test.png"));
+
+        webDriver.quit();
     }
 
 }
