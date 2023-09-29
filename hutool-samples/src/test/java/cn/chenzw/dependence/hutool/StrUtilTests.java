fe4159437c0cba7bc208433d52d5dbad0c9a32f2
chenzw <ffchenzw@chinatelecom.cn>
2023-09-29 21:10:47 +0800

    update

---
+++ hutool-samples/src/test/java/cn/chenzw/dependence/hutool/StrUtilTests.java
@@ -0,0 +1,20 @@
+package cn.chenzw.dependence.hutool;
+
+import cn.hutool.core.util.StrUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/**
+ * @author chenzw
+ */
+@Slf4j
+@RunWith(JUnit4.class)
+public class StrUtilTests {
+
+    @Test
+    public void test() {
+
+    }
+}
