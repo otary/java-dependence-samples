83bcf4cbfd27873470ea117a90df2ad2b8ce7a0a
chenzw2 <ffchenzw@chinatelecom.cn>
2024-04-30 15:34:20 +0800

    update

--- acme4j-samples/src/test/java/cn/chenzw/acme4j/Acme4jTests.java
+++ acme4j-samples/src/test/java/cn/chenzw/acme4j/Acme4jTests.java
@@ -2,23 +2,28 @@ package cn.chenzw.acme4j;
 
 import lombok.extern.slf4j.Slf4j;
 import org.bouncycastle.asn1.cmp.Challenge;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 import org.shredzone.acme4j.*;
+import org.shredzone.acme4j.challenge.Dns01Challenge;
 import org.shredzone.acme4j.challenge.Http01Challenge;
 import org.shredzone.acme4j.exception.AcmeException;
+import org.shredzone.acme4j.provider.ChallengeType;
+import org.shredzone.acme4j.util.CSRBuilder;
 import org.shredzone.acme4j.util.KeyPairUtils;
 import org.springframework.beans.factory.parsing.Problem;
 
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import javax.swing.*;
+import java.io.*;
 import java.net.URI;
 import java.security.KeyPair;
-import java.util.Collection;
-import java.util.Optional;
+import java.security.Security;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.*;
+import java.util.function.Supplier;
 
 /**
  * @author chenzw
@@ -27,74 +32,192 @@ import java.util.Optional;
 @RunWith(JUnit4.class)
 public class Acme4jTests {
 
-    @Test
-    public void test() {
-        if (USER_KEY_FILE.exists()) {
-            // If there is a key file, read it
-            try (FileReader fr = new FileReader(USER_KEY_FILE)) {
-                return KeyPairUtils.readKeyPair(fr);
-            }
-        } else {
-            // If there is none, create a new key pair and save it
-            KeyPair userKeyPair = ACCOUNT_KEY_SUPPLIER.get();
-            try (FileWriter fw = new FileWriter(USER_KEY_FILE)) {
-                KeyPairUtils.writeKeyPair(userKeyPair, fw);
-            }
-            return userKeyPair;
-        }
+    // 申请的域名
+    private List<String> domains = Arrays.asList("yl.bqrdh.com");
 
+    static {
+        try {
+            Security.addProvider(new BouncyCastleProvider());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
-
-
-  /*  private static final File USER_KEY_FILE = new File("user.key");
-
-    private static final String CA_URI = "acme://example.com/staging";
-
-
-
-
-
-    *//**
-     * 抓取指定域名的证书
+    /**
      *
-     * @param domains
      * @throws IOException
      * @throws AcmeException
-     *//*
-    public void fetchCertificate(Collection<String> domains) throws IOException, AcmeException {
-        // Load the user key file. If there is no key file, create a new one.
-        KeyPair userKeyPair = loadOrCreateUserKeyPair();
+     */
+    @Test
+    public void test() throws IOException, AcmeException {
 
-        // Create a session.
-        Session session = new Session(CA_URI);
+        KeyPair userKeyPair = KeyPairUtils.createKeyPair();
 
-        // Get the Account.
-        // If there is no account yet, create a new one.
-        Account acct = findOrRegisterAccount(session, userKeyPair);
+        /**
+         * {
+         *   "keyChange": "https://acme-v02.api.letsencrypt.org/acme/key-change",
+         *   "meta": {
+         *     "caaIdentities": [
+         *       "letsencrypt.org"
+         *     ],
+         *     "termsOfService": "https://letsencrypt.org/documents/LE-SA-v1.4-April-3-2024.pdf",
+         *     "website": "https://letsencrypt.org"
+         *   },
+         *   "muL7lJGjzo0": "https://community.letsencrypt.org/t/adding-random-entries-to-the-directory/33417",
+         *   "newAccount": "https://acme-v02.api.letsencrypt.org/acme/new-acct",
+         *   "newNonce": "https://acme-v02.api.letsencrypt.org/acme/new-nonce",
+         *   "newOrder": "https://acme-v02.api.letsencrypt.org/acme/new-order",
+         *   "renewalInfo": "https://acme-v02.api.letsencrypt.org/draft-ietf-acme-ari-02/renewalInfo/",
+         *   "revokeCert": "https://acme-v02.api.letsencrypt.org/acme/revoke-cert"
+         * }
+         */
+        Session session = new Session("https://acme-v02.api.letsencrypt.org/directory");
 
-        // Load or create a key pair for the domains. This should not be the userKeyPair!
-        KeyPair domainKeyPair = loadOrCreateDomainKeyPair();
+        // 用于提示（Do you accept the Terms of Service?）
+        // URI toc = session.getMetadata().getTermsOfService();
 
-        // Order the certificate
-        Order order = acct.newOrder().domains(domains).create();
+        AccountBuilder accountBuilder = new AccountBuilder()
+                .agreeToTermsOfService()
+                .useKeyPair(userKeyPair);
 
-        // Perform all required authorizations
+        // 非必填？
+        accountBuilder.addEmail("656469722@qq.com");
+
+        // Use the KID and HMAC if the CA uses External Account Binding
+        // accountBuilder.withKeyIdentifier(EAB_KID, EAB_HMAC);
+
+        // 创建账号
+        Account account = accountBuilder.create(session);
+
+        // 域名密钥对
+        KeyPair domainKeyPair = KeyPairUtils.createKeyPair(4096);
+
+        // 订购证书
+        Order order = account.newOrder().domains(domains).create();
+
+        // 授权
         for (Authorization auth : order.getAuthorizations()) {
-            authorize(auth);
+            if (auth.getStatus() == Status.VALID) {
+                return;
+            }
+
+            // 方式一：HTTP方式质询
+            Http01Challenge challenge = auth.findChallenge(Http01Challenge.class);
+            if (challenge == null) {
+                throw new AcmeException("Found no " + Http01Challenge.TYPE + " challenge, don't know what to do...");
+            }
+
+            log.info("Please create a file in your web server's base directory.");
+            log.info("It must be reachable at: http://{}/.well-known/acme-challenge/{}",
+                    auth.getIdentifier().getDomain(), challenge.getToken());
+            log.info("File name: {}", challenge.getToken());
+            log.info("Content: {}", challenge.getAuthorization());
+            log.info("The file must not contain any leading or trailing whitespaces or line breaks!");
+            log.info("If you're ready, dismiss the dialog...");
+
+            StringBuilder message = new StringBuilder();
+            message.append("Please create a file in your web server's base directory.\n\n");
+            message.append("http://")
+                    .append(auth.getIdentifier().getDomain())
+                    .append("/.well-known/acme-challenge/")
+                    .append(challenge.getToken())
+                    .append("\n\n");
+            message.append("Content:\n\n");
+            message.append(challenge.getAuthorization());
+            log.info("message => {}", message);
+
+            // 此时需要手动将content内容写入到acme-challenge下的文件中
+
+
+            // 方式二：DNS方式质询
+            /*
+            Dns01Challenge challenge = auth.findChallenge(Dns01Challenge.TYPE)
+            if (challenge == null) {
+                throw new AcmeException("Found no " + Http01Challenge.TYPE + " challenge, don't know what to do...");
+            }
+            log.info("Please create a TXT record:");
+            log.info("{} IN TXT {}",
+                    Dns01Challenge.toRRName(auth.getIdentifier()), challenge.getDigest());
+            log.info("If you're ready, dismiss the dialog...");
+
+            StringBuilder message = new StringBuilder();
+            message.append("Please create a TXT record:\n\n");
+            message.append(Dns01Challenge.toRRName(auth.getIdentifier()))
+                    .append(" IN TXT ")
+                    .append(challenge.getDigest());
+            log.info("message => {}", message);
+            */
+
+            if (challenge == null) {
+                throw new AcmeException("No challenge found");
+            }
+            if (challenge.getStatus() == Status.VALID) {
+                return;
+            }
+            // 开始质询
+            challenge.trigger();
+
+            try {
+                int attempts = 10;
+                while (challenge.getStatus() != Status.VALID && attempts-- > 0) {
+                    // Did the authorization fail?
+                    if (challenge.getStatus() == Status.INVALID) {
+                        log.error("Challenge has failed, reason: {}", challenge.getError());
+                        throw new AcmeException("Challenge failed... Giving up.");
+                    }
+
+                    // Wait for a few seconds
+                    Thread.sleep(3000L);
+
+                    // Then update the status
+                    challenge.update();
+                }
+            } catch (InterruptedException ex) {
+                log.error("interrupted", ex);
+                Thread.currentThread().interrupt();
+            }
+
+            if (challenge.getStatus() != Status.VALID) {
+                throw new AcmeException("Failed to pass the challenge for domain "
+                        + auth.getIdentifier().getDomain() + ", ... Giving up.");
+            }
+
+            log.info("Challenge has been completed. Remember to remove the validation resource.");
         }
 
-        // Order the certificate
-        order.execute(domainKeyPair);
+        // Generate a CSR for all of the domains, and sign it with the domain key pair.
+        CSRBuilder csrb = new CSRBuilder();
+        csrb.addDomains(domains);
+        csrb.sign(domainKeyPair);
+
+        /*
+        try (Writer out = new FileWriter(DOMAIN_CSR_FILE)) {
+            csrb.write(out);
+        }
+        */
+
+        // 发起订购
+        order.execute(csrb.getEncoded());
 
         // Wait for the order to complete
-        Status status = waitForCompletion(order::getStatus, order::fetch);
-        if (status != Status.VALID) {
-            LOG.error("Order has failed, reason: {}", order.getError()
-                    .map(Problem::toString)
-                    .orElse("unknown")
-            );
-            throw new AcmeException("Order failed... Giving up.");
+        try {
+            int attempts = 10;
+            while (order.getStatus() != Status.VALID && attempts-- > 0) {
+                // Did the order fail?
+                if (order.getStatus() == Status.INVALID) {
+                    log.error("Order has failed, reason: {}", order.getError());
+                    throw new AcmeException("Order failed... Giving up.");
+                }
+
+                // Wait for a few seconds
+                Thread.sleep(3000L);
+
+                // Then update the status
+                order.update();
+            }
+        } catch (InterruptedException ex) {
+            log.error("interrupted", ex);
+            Thread.currentThread().interrupt();
         }
 
         // Get the certificate
@@ -104,155 +227,10 @@ public class Acme4jTests {
         log.info("Certificate URL: {}", certificate.getLocation());
 
         // Write a combined file containing the certificate and chain.
-        try (FileWriter fw = new FileWriter(DOMAIN_CHAIN_FILE)) {
+        try (FileWriter fw = new FileWriter(new File("domain-chain.crt"))) {
             certificate.writeCertificate(fw);
         }
 
-        // That's all! Configure your web server to use the DOMAIN_KEY_FILE and
-        // DOMAIN_CHAIN_FILE for the requested domains.
     }
-
-    *//**
-     * 加载或生成用户密钥对
-     *
-     * @return
-     * @throws IOException
-     *//*
-    private KeyPair loadOrCreateUserKeyPair() throws IOException {
-        if (USER_KEY_FILE.exists()) {
-            // If there is a key file, read it
-            try (FileReader fr = new FileReader(USER_KEY_FILE)) {
-                return KeyPairUtils.readKeyPair(fr);
-            }
-        } else {
-            // If there is none, create a new key pair and save it
-            KeyPair userKeyPair = ACCOUNT_KEY_SUPPLIER.get();
-            try (FileWriter fw = new FileWriter(USER_KEY_FILE)) {
-                KeyPairUtils.writeKeyPair(userKeyPair, fw);
-            }
-            return userKeyPair;
-        }
-    }
-
-    *//**
-     * 查找或注册账号（ACME服务器）
-     *
-     * @param session
-     * @param accountKey
-     * @return
-     * @throws AcmeException
-     *//*
-    private Account findOrRegisterAccount(Session session, KeyPair accountKey) throws AcmeException {
-        // Ask the user to accept the TOS, if server provides us with a link.
-        Optional<URI> tos = session.getMetadata().getTermsOfService();
-        if (tos.isPresent()) {
-            acceptAgreement(tos.get());
-        }
-
-        AccountBuilder accountBuilder = new AccountBuilder()
-                .agreeToTermsOfService()
-                .useKeyPair(accountKey);
-
-        // Set your email (if available)
-        if (ACCOUNT_EMAIL != null) {
-            accountBuilder.addEmail(ACCOUNT_EMAIL);
-        }
-
-        // Use the KID and HMAC if the CA uses External Account Binding
-        if (EAB_KID != null && EAB_HMAC != null) {
-            accountBuilder.withKeyIdentifier(EAB_KID, EAB_HMAC);
-        }
-
-        Account account = accountBuilder.create(session);
-        log.info("Registered a new user, URL: {}", account.getLocation());
-
-        return account;
-    }
-
-    *//**
-     * 域名授权 - 账号与域名进行关联
-     *
-     * @param auth
-     * @throws AcmeException
-     *//*
-    private void authorize(Authorization auth) throws AcmeException {
-        log.info("Authorization for domain {}", auth.getIdentifier().getDomain());
-
-        // The authorization is already valid. No need to process a challenge.
-        if (auth.getStatus() == Status.VALID) {
-            return;
-        }
-
-        // Find the desired challenge and prepare it.
-        Challenge challenge = null;
-        switch (CHALLENGE_TYPE) {
-            case HTTP:
-                challenge = httpChallenge(auth);
-                break;
-
-            case DNS:
-                challenge = dnsChallenge(auth);
-                break;
-        }
-
-        if (challenge == null) {
-            throw new AcmeException("No challenge found");
-        }
-
-        // If the challenge is already verified, there's no need to execute it again.
-        if (challenge.getStatus() == Status.VALID) {
-            return;
-        }
-
-        // Now trigger the challenge.
-        challenge.trigger();
-
-        // Poll for the challenge to complete.
-        Status status = waitForCompletion(challenge::getStatus, challenge::fetch);
-        if (status != Status.VALID) {
-            LOG.error("Challenge has failed, reason: {}", challenge.getError()
-                    .map(Problem::toString)
-                    .orElse("unknown"));
-            throw new AcmeException("Challenge failed... Giving up.");
-        }
-
-        log.info("Challenge has been completed. Remember to remove the validation resource.");
-        completeChallenge("Challenge has been completed.\nYou can remove the resource again now.");
-    }
-
-    *//**
-     * 准备HTTP质询
-     *
-     * @param auth
-     * @return
-     * @throws AcmeException
-     *//*
-    public Challenge httpChallenge(Authorization auth) throws AcmeException {
-        // Find a single http-01 challenge
-        Http01Challenge challenge = auth.findChallenge(Http01Challenge.class)
-                .orElseThrow(() -> new AcmeException("Found no " + Http01Challenge.TYPE
-                        + " challenge, don't know what to do..."));
-
-        // Output the challenge, wait for acknowledge...
-        log.info("Please create a file in your web server's base directory.");
-        log.info("It must be reachable at: http://{}/.well-known/acme-challenge/{}",
-                auth.getIdentifier().getDomain(), challenge.getToken());
-        log.info("File name: {}", challenge.getToken());
-        log.info("Content: {}", challenge.getAuthorization());
-        log.info("The file must not contain any leading or trailing whitespaces or line breaks!");
-        log.info("If you're ready, dismiss the dialog...");
-
-        StringBuilder message = new StringBuilder();
-        message.append("Please create a file in your web server's base directory.\n\n");
-        message.append("http://")
-                .append(auth.getIdentifier().getDomain())
-                .append("/.well-known/acme-challenge/")
-                .append(challenge.getToken())
-                .append("\n\n");
-        message.append("Content:\n\n");
-        message.append(challenge.getAuthorization());
-        acceptChallenge(message.toString());
-        return challenge;
-    }*/
 
 }
