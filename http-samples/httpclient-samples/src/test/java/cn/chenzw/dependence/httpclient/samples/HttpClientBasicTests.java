e542ae3f957f678999669552d4912a1994212bf8
chenzw <656469722@qq.com>
2024-12-16 08:48:48 +0800

    update

--- http-samples/httpclient-samples/src/test/java/cn/chenzw/dependence/httpclient/samples/HttpClientBasicTests.java
+++ http-samples/httpclient-samples/src/test/java/cn/chenzw/dependence/httpclient/samples/HttpClientBasicTests.java
@@ -22,31 +22,30 @@ public class HttpClientBasicTests {
 
     @Test
     public void testGet() {
-        CloseableHttpClient httpClient = HttpClients.createDefault();
-        HttpGet httpGet = new HttpGet("http://www.baidu.com");
-        try (CloseableHttpResponse response = httpClient.execute(httpGet)) {
-            Assert.assertEquals(200, response.getStatusLine().getStatusCode());
+        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
+            HttpGet httpGet = new HttpGet("http://www.baidu.com");
+            try (CloseableHttpResponse response = httpClient.execute(httpGet)) {
+                Assert.assertEquals(200, response.getStatusLine().getStatusCode());
+                String body = EntityUtils.toString(response.getEntity());
+                log.info("body => {}", body);
+            }
         } catch (IOException e) {
             e.printStackTrace();
-        } finally {
-            try {
-                httpClient.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
         }
     }
 
     @Test
-    public void testPost() throws IOException {
-        CloseableHttpClient httpClient = HttpClients.createDefault();
-        HttpPost httpPost = new HttpPost("https://www.baidu.com");
-        httpPost.setEntity(new StringEntity("{}", ContentType.create("application/json", "utf-8")));
-        try (CloseableHttpResponse response = httpClient.execute(httpPost)) {
-            String body = EntityUtils.toString(response.getEntity());
-
-            log.info(" => {}", body);
+    public void testPost() {
+        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
+            HttpPost httpPost = new HttpPost("https://www.baidu.com");
+            httpPost.setEntity(new StringEntity("{}", ContentType.create("application/json", "utf-8")));
+            try (CloseableHttpResponse response = httpClient.execute(httpPost)) {
+                String body = EntityUtils.toString(response.getEntity());
+                log.info("body => {}", body);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-
     }
+
 }
